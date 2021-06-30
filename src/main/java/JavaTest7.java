3b0bb47e86aa041764cf16fdf251d232e86bf080
MQcapcured <mqcaptured@gmail.com>
2021-06-30 18:00:38 +0800

    add branch dev-java7-java8 add file java test 7 & 8

---
+++ src/main/java/JavaTest7.java
@@ -0,0 +1,9 @@
+/**
+ * @description: java test 7 for branch test
+ * @author: MQcaptured
+ * @create: 2021-06-30
+ **/
+
+public class JavaTest7 {
+
+}
---
+++ src/main/java/JavaTest8.java
@@ -0,0 +1,9 @@
+/**
+ * @description: java test 8 for branch test
+ * @author: MQcaptured
+ * @create: 2021-06-30
+ **/
+
+public class JavaTest8 {
+
+}
