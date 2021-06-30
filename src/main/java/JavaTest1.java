76f3ae1014aa317c13f4baff00b8cbf60ca6f41e
MQcapcured <mqcaptured@gmail.com>
2021-06-30 17:54:50 +0800

    add file java test 1 & 2

---
+++ src/main/java/JavaTest1.java
@@ -0,0 +1,9 @@
+/**
+ * @description: java test 1
+ * @author: MQcaptured
+ * @create: 2021-06-30
+ **/
+
+public class JavaTest1 {
+
+}
---
+++ src/main/java/JavaTest2.java
@@ -0,0 +1,9 @@
+/**
+ * @description: java test 2
+ * @author: MQcaptured
+ * @create: 2021-06-30
+ **/
+
+public class JavaTest2 {
+
+}
