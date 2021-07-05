f490e08e87e9ac70f96e337d9b483f3baf60eaaa
MQcapcured <mqcaptured@gmail.com>
2021-07-05 22:29:32 +0800

    create JavaTest3 for merging branch test

---
+++ src/main/java/JavaTest3.java
@@ -0,0 +1,9 @@
+/**
+ * @description: create for merging branch test
+ * @author: MQcaptured
+ * @create: 2021-07-05
+ **/
+
+public class JavaTest3 {
+    // add a local line for merging branches test
+}
